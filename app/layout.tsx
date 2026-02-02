3ada642208e9f0783d0770583685dbc01df8a0b9
Nithin <10778861+darthnithin@users.noreply.github.com>
2026-02-01 16:22:13 -0800

    navbar should not be child of providers (for weird react reasons)

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Navbar />
         <Providers>
-          <Navbar />
           <main>{children}</main>
         </Providers>
       </body>
